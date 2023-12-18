588bc313b012ac26f25383f218752467e99167d6
asari <32879431+deomorxsy@users.noreply.github.com>
2023-12-18 18:49:10 -0300

    fix circular dependency hell ;D [3/3]; change database URI environment variable from the config.py file fix script to run the backend server from the project root todo: test database endpoints

--- scripts/run-backend.sh
+++ scripts/run-backend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 export DB_URL="postgresql://postgres:admin@localhost:5432/db"
+export FLASK_APP="app"
 source ../venv/bin/activate
 cd ./backend/ || exit
 flask --app app run --debug
